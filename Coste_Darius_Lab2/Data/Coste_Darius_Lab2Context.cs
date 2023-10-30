fe58d72ed13d4d69e1643f012daab4523e6b81d1
dariuscoste <dcoste05@gmail.com>
2023-10-30 19:31:00 +0200

    Add project files.

---
+++ Coste_Darius_Lab2/Data/Coste_Darius_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Coste_Darius_Lab2.Models;
+
+namespace Coste_Darius_Lab2.Data
+{
+    public class Coste_Darius_Lab2Context : DbContext
+    {
+        public Coste_Darius_Lab2Context (DbContextOptions<Coste_Darius_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coste_Darius_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Coste_Darius_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
